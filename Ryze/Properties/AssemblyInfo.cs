781a6c906653e9e427cbdaffcb66089825ec48eb
elolam <sdtofma@gmail.com>
2016-03-14 21:40:20 +0700

    Update 6.5

--- Ryze/Properties/AssemblyInfo.cs
+++ Ryze/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Tristana")]
+[assembly: AssemblyTitle("Ryze")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tristana")]
-[assembly: AssemblyCopyright("Copyright © Haker 2015")]
+[assembly: AssemblyProduct("Ryze")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3203502a-4253-4edd-9331-74a49c616ed2")]
+[assembly: Guid("d73402a1-79dc-4c16-a5eb-745682441147")]
 
 // Version information for an assembly consists of the following four values:
 //
