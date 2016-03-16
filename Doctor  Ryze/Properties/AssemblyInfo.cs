cead05539c6e0fe9f1421a7c52841944a1c8dcee
elolam <sdtofma@gmail.com>
2016-03-17 04:38:22 +0700

    yup

--- Doctor  Ryze/Properties/AssemblyInfo.cs
+++ Doctor  Ryze/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ryze")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  Doctor")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
